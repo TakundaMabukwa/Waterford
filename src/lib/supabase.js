b6321c200ebac9ce397b78148394deea9117ed9e
TakundaMabukwa <mabukwatakunda@gmail.com>
2025-12-11 21:39:20 +0200

    ....

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,17 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
+let _supabase = null
 
-export const supabase = supabaseUrl && supabaseKey ? createClient(supabaseUrl, supabaseKey) : null
+export const supabase = (() => {
+  if (!_supabase) {
+    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+    const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    
+    if (supabaseUrl && supabaseKey) {
+      _supabase = createClient(supabaseUrl, supabaseKey, {
+        auth: { persistSession: false }
+      })
+    }
+  }
+  return _supabase
+})()
