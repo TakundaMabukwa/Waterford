197fe8a1997bb4e58cdaad85261103d5b96c7e48
TakundaMabukwa <mabukwatakunda@gmail.com>
2025-12-20 18:43:33 +0200

    ...

--- src/app/(auth)/logout/page.tsx
+++ src/app/(auth)/logout/page.tsx
@@ -1,6 +1,8 @@
 import { logout } from "@/lib/action/logout"
 import { redirect } from "next/navigation"
 
+export const dynamic = 'force-dynamic'
+
 export default async function LogoutPage() {
   await logout()
   return redirect('/login') 
