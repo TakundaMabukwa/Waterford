83bf72ff62b4b04333acb944dd7d544a24c71c78
TakundaMabukwa <mabukwatakunda@gmail.com>
2026-01-05 17:05:25 +0200

    pm2 local

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,22 +3,11 @@ module.exports = {
     name: 'eps-dashboard',
     script: 'npm',
     args: 'start',
-    instances: 1,
-    exec_mode: 'fork',
-    max_memory_restart: '1.5G',
-    node_args: '--max-old-space-size=1536',
+    cwd: '/var/www/eps/EPS-Dashboard',
     env: {
       NODE_ENV: 'production',
-      PORT: 3000
+      PORT: 3001
     },
-    log_date_format: 'YYYY-MM-DD HH:mm:ss',
-    error_file: './logs/err.log',
-    out_file: './logs/out.log',
-    log_file: './logs/combined.log',
-    time: true,
-    autorestart: true,
-    watch: false,
-    max_restarts: 10,
-    min_uptime: '10s'
+    env_file: '.env.local'
   }]
 }
